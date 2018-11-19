f3e928aa79f75d5e1103bfdc686e17bb70863492
Ido Yehezkel <idoye@campus.technion.ac.il>
2018-11-19 19:51:25 +0200

    ex 20

--- HW_1/experiments/temperature.py
+++ HW_1/experiments/temperature.py
@@ -3,10 +3,22 @@ from matplotlib import pyplot as plt
 
 X = np.array([400, 450, 900, 390, 550])
 
-# TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+T = np.linspace(0.01, 5, 100)
 
-print(P)
+P = np.zeros((len(T), len(X)))
+
+SUM_T = np.zeros((len(T)))
+for t_index, t_value in enumerate(T):
+    pw = -float(1 / t_value)
+    SUM_T[t_index] = np.sum(X ** pw)
+
+for x_index, x_value in enumerate(X):
+    numerator = np.zeros(len(T))
+    for t_index, t_value in enumerate(T):
+        pw = -float(1 / t_value)
+        numerator[t_index] = x_value ** pw
+
+    P[:, x_index] = numerator / SUM_T[:]
 
 for i in range(len(X)):
     plt.plot(T, P[:, i], label=str(X[i]))
