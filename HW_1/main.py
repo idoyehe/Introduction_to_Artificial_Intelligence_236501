9a34d642a8ce513cf0645e978f83f214d0df4897
Ido Yehezkel <idoye@campus.technion.ac.il>
2018-11-21 20:05:39 +0200

    main ready for submission

--- HW_1/main.py
+++ HW_1/main.py
@@ -201,22 +201,18 @@ def strict_deliveries_problem():
     # Ex.26
     # Call here the function `run_astar_for_weights_in_range()`
     # with `MSTAirDistHeuristic` and `small_deliveries_prob`.
-    # run_astar_for_weights_in_range(MSTAirDistHeuristic, small_deliveries_strict_problem)
+    run_astar_for_weights_in_range(MSTAirDistHeuristic, small_deliveries_strict_problem)
 
     # Ex.28
     # an instance of `AStar` with the `RelaxedDeliveriesHeuristic`,
     # solve the `small_deliveries_strict_problem` with it and print the results (as before).
-    astr28 = AStar(MSTAirDistHeuristic)
-    res28 = astr28.solve_problem(small_deliveries_strict_problem)
-    print(res28)
-
     astr28 = AStar(RelaxedDeliveriesHeuristic)
     res28 = astr28.solve_problem(small_deliveries_strict_problem)
     print(res28)
 
 def main():
-    # map_problem()
-    # relaxed_deliveries_problem()
+    map_problem()
+    relaxed_deliveries_problem()
     strict_deliveries_problem()
 
 
