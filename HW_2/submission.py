4f97f40787445692a2248c3fbc331a989d4c6be0
idoye <idoye@campus.technion.ac.il>
2018-12-11 20:22:46 +0200

    first commit

--- HW_2/submission.py
+++ HW_2/submission.py
@@ -1,58 +1,59 @@
 import random, util
 from game import Agent
 
+
 #     ********* Reflex agent- sections a and b *********
 class ReflexAgent(Agent):
-  """
+    """
     A reflex agent chooses an action at each choice point by examining
     its alternatives via a state evaluation function.
   """
-  def __init__(self):
-    self.lastPositions = []
-    self.dc = None
 
+    def __init__(self):
+        self.lastPositions = []
+        self.dc = None
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
     getAction chooses among the best options according to the evaluation function.
 
     getAction takes a GameState and returns some Directions.X for some X in the set {North, South, West, East, Stop}
     ------------------------------------------------------------------------------
     """
-    # Collect legal moves and successor states
-    legalMoves = gameState.getLegalActions()
+        # Collect legal moves and successor states
+        legalMoves = gameState.getLegalActions()
 
-    # Choose one of the best actions
-    scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
-    bestScore = max(scores)
-    bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-    chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+        # Choose one of the best actions
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
+        return legalMoves[chosenIndex]
 
-    return legalMoves[chosenIndex]
-
-  def evaluationFunction(self, currentGameState, action):
-    """
+    def evaluationFunction(self, currentGameState, action):
+        """
     The evaluation function takes in the current GameState (pacman.py) and the proposed action
     and returns a number, where higher numbers are better.
     """
-    successorGameState = currentGameState.generatePacmanSuccessor(action)
-    return scoreEvaluationFunction(successorGameState)
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        return scoreEvaluationFunction(successorGameState)
 
 
 #     ********* Evaluation functions *********
 
 def scoreEvaluationFunction(gameState):
-  """
+    """
     This default evaluation function just returns the score of the state.
     The score is the same one displayed in the Pacman GUI.
   """
-  return gameState.getScore()
+    return gameState.getScore()
+
 
 ######################################################################################
 # b: implementing a better heuristic function
 def betterEvaluationFunction(gameState):
-  """
+    """
 
   The betterEvaluationFunction takes in a GameState (pacman.py) and should return a number, where higher numbers are better.
 
@@ -68,10 +69,11 @@ def betterEvaluationFunction(gameState):
   The GameState class is defined in pacman.py and you might want to look into that for other helper methods.
   """
 
+
 #     ********* MultiAgent Search Agents- sections c,d,e,f*********
 
 class MultiAgentSearchAgent(Agent):
-  """
+    """
     This class provides some common elements to all of your
     multi-agent searchers.  Any methods defined here will be available
     to the MinimaxAgent, AlphaBetaAgent & both ExpectimaxAgents.
@@ -85,21 +87,22 @@ class MultiAgentSearchAgent(Agent):
     is another abstract class.
   """
 
-  def __init__(self, evalFn = 'betterEvaluationFunction', depth = '2'):
-    self.index = 0 # Pacman is always agent index 0
-    self.evaluationFunction = util.lookup(evalFn, globals())
-    self.depth = int(depth)
+    def __init__(self, evalFn='betterEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
+        self.evaluationFunction = util.lookup(evalFn, globals())
+        self.depth = int(depth)
+
 
 ######################################################################################
 # c: implementing minimax
 
 class MinimaxAgent(MultiAgentSearchAgent):
-  """
+    """
     Your minimax agent
   """
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
       Returns the minimax action from the current gameState using self.depth
       and self.evaluationFunction. Terminal states can be found by one of the following:
       pacman won, pacman lost or there are no legal moves.
@@ -133,81 +136,81 @@ class MinimaxAgent(MultiAgentSearchAgent):
 
     """
 
-    # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+        # BEGIN_YOUR_CODE
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
 
 ######################################################################################
 # d: implementing alpha-beta
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
-  """
+    """
     Your minimax agent with alpha-beta pruning
   """
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
       Returns the minimax action using self.depth and self.evaluationFunction
     """
 
-    # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+        # BEGIN_YOUR_CODE
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
 
 ######################################################################################
 # e: implementing random expectimax
 
 class RandomExpectimaxAgent(MultiAgentSearchAgent):
-  """
+    """
     Your expectimax agent
   """
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
       Returns the expectimax action using self.depth and self.evaluationFunction
       All ghosts should be modeled as choosing uniformly at random from their legal moves.
     """
 
-    # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+        # BEGIN_YOUR_CODE
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
 
 ######################################################################################
 # f: implementing directional expectimax
 
 class DirectionalExpectimaxAgent(MultiAgentSearchAgent):
-  """
+    """
     Your expectimax agent
   """
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
       Returns the expectimax action using self.depth and self.evaluationFunction
       All ghosts should be modeled as using the DirectionalGhost distribution to choose from their legal moves.
     """
 
-    # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+        # BEGIN_YOUR_CODE
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
 
 
 ######################################################################################
 # I: implementing competition agent
 
 class CompetitionAgent(MultiAgentSearchAgent):
-  """
+    """
     Your competition agent
   """
 
-  def getAction(self, gameState):
-    """
+    def getAction(self, gameState):
+        """
       Returns the action using self.depth and self.evaluationFunction
 
     """
 
-    # BEGIN_YOUR_CODE
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
-
-
-
+        # BEGIN_YOUR_CODE
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
