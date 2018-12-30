689738435ef897547aa28aad2fa6b97e7a5cc0e3
Ido Yehezkel <idoye@campus.technion.ac.il>
2018-12-30 22:11:38 +0200

    minor change

--- HW_2/CompetitionAgentEvaluation.py
+++ HW_2/CompetitionAgentEvaluation.py
@@ -1,5 +1,4 @@
 from pacman import runGames, readCommand
-from submission import *
 from time import time
 layout_list = [
     "originalClassic",
@@ -14,20 +13,15 @@ layout_list = [
     "capsuleClassic"
 ]
 
-# curr_layout = layout_list[3]
-
 ghost_agents = ["DirectionalGhost", "RandomGhost"]
 
 for curr_layout in layout_list:
     print("Current layout: ", curr_layout)
-    currentAgent = CompetitionAgent()
-    agent_str = "CompetitionAgent"
-    n = 10
-    args = readCommand(["-n", str(n), "-k", "2", "-l", curr_layout, "-p", agent_str, "-q", "-g", ghost_agents[1]])  # Get game components based on input
-    args['pacman'] = currentAgent
+    games_counter = 10
+    args = readCommand(["-n", str(games_counter), "-k", "2", "-l", curr_layout, "-p", "CompetitionAgent", "-q", "-g", ghost_agents[1]])  # Get game components based on input
     start = time()
     avg = runGames(**args)
-    print("\n")
     end = time()
-    print("Current layout AVG completion time: ", float(end - start) / float(n))
+    print("\n")
+    print("Current layout AVG completion time: ", float(end - start) / float(games_counter))
     print("\n")
